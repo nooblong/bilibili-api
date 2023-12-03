75d3556ffab2edf6fed200c2b6ad202e65e91c50
龙打野 <18128966990@163.com>
2023-12-03 19:50:46 +0800

    Update server.py

--- server.py
+++ server.py
@@ -27,81 +27,104 @@ def enum_to_value(obj):
 
 @app.get("/<package:str>/<clazz:str>/<func:str>")
 async def req_by_clazz(request, package, clazz, func):
-    package_attr = getattr(sys.modules[__name__], package)
-    clazz_attr = getattr(package_attr, clazz)
-    clazz_params = inspect.signature(clazz_attr).parameters
-    cred_obj = None
-    cred_dict = dict()
-    clazz_dict = dict()
-    for i in request.args:
-        if i == "sessdata" or i == "bili_jct" or i == "buvid3" or i == "ac_time_value":
-            cred_dict[i] = request.args[i][0]
-            # continue
-        if i in clazz_params.keys():
-            clazz_dict[i] = request.args[i][0]
+    try:
+        package_attr = getattr(sys.modules[__name__], package)
+        clazz_attr = getattr(package_attr, clazz)
+        clazz_params = inspect.signature(clazz_attr).parameters
+        cred_obj = None
+        cred_dict = dict()
+        clazz_dict = dict()
+        for i in request.args:
+            if i == "sessdata" or i == "bili_jct" or i == "buvid3" or i == "ac_time_value":
+                cred_dict[i] = request.args[i][0]
+                # continue
+            if i in clazz_params.keys():
+                clazz_dict[i] = request.args[i][0]
 
-    special_param(clazz_dict, clazz)
+        special_param(clazz_dict, clazz)
 
-    if cred_dict:
-        cred_obj = Credential(**cred_dict)
+        if cred_dict:
+            cred_obj = Credential(**cred_dict)
 
-    if "credential" in clazz_params.keys():
-        clazz_dict["credential"] = cred_obj
+        if "credential" in clazz_params.keys():
+            clazz_dict["credential"] = cred_obj
 
-    func_dict = dict()
-    clazz_obj = clazz_attr(**clazz_dict)
-    func_attr = getattr(clazz_obj, func)
-    func_params = inspect.signature(func_attr).parameters
+        func_dict = dict()
+        clazz_obj = clazz_attr(**clazz_dict)
+        func_attr = getattr(clazz_obj, func)
+        func_params = inspect.signature(func_attr).parameters
 
-    if "credential" in func_params.keys():
-        func_dict["credential"] = cred_obj
+        if "credential" in func_params.keys():
+            func_dict["credential"] = cred_obj
 
-    for i in request.args:
-        if i in func_params.keys():
-            func_dict[i] = request.args[i][0]
+        for i in request.args:
+            if i in func_params.keys():
+                func_dict[i] = request.args[i][0]
 
-    special_param(func_dict, func)
+        special_param(func_dict, func)
 
-    if inspect.isasyncgenfunction(func_attr) or inspect.iscoroutinefunction(func_attr):
-        result = await func_attr(**func_dict)
-    else:
-        result = func_attr(**func_dict)
-    return json(enum_to_value(result))
+        if inspect.isasyncgenfunction(func_attr) or inspect.iscoroutinefunction(func_attr):
+            result = await func_attr(**func_dict)
+        else:
+            result = func_attr(**func_dict)
+        result_no_enum = enum_to_value(result)
+
+    except Exception as e:
+        wrap_result = dict()
+        wrap_result["code"] = -1
+        wrap_result["message"] = str(e)
+        return json(wrap_result)
+
+    wrap_result = dict()
+    wrap_result["code"] = 0
+    wrap_result["data"] = result_no_enum
+    return json(wrap_result)
 
 
 @app.get("/<package:str>/<func:str>")
 async def req_by_static(request, package, func):
-    package_attr = getattr(sys.modules[__name__], package)
-    func_attr = getattr(package_attr, func)
-    func_params = inspect.signature(func_attr).parameters
-    cred_obj = None
-    cred_dict = dict()
-    func_dict = dict()
+    try:
+        package_attr = getattr(sys.modules[__name__], package)
+        func_attr = getattr(package_attr, func)
+        func_params = inspect.signature(func_attr).parameters
+        cred_obj = None
+        cred_dict = dict()
+        func_dict = dict()
 
-    for i in request.args:
-        if i == "sessdata" or i == "bili_jct" or i == "buvid3" or i == "ac_time_value":
-            cred_dict[i] = request.args[i][0]
-            # continue
-        if i in func_params:
-            func_dict[i] = request.args[i][0]
+        for i in request.args:
+            if i == "sessdata" or i == "bili_jct" or i == "buvid3" or i == "ac_time_value":
+                cred_dict[i] = request.args[i][0]
+                # continue
+            if i in func_params:
+                func_dict[i] = request.args[i][0]
 
-    if cred_dict:
-        cred_obj = Credential(**cred_dict)
+        if cred_dict:
+            cred_obj = Credential(**cred_dict)
 
-    if package == "Credential":
-        func_attr = getattr(cred_obj, func)
+        if package == "Credential":
+            func_attr = getattr(cred_obj, func)
 
-    if "credential" in func_params.keys():
-        func_dict["credential"] = cred_obj
+        if "credential" in func_params.keys():
+            func_dict["credential"] = cred_obj
 
-    special_param(func_dict, package)
+        special_param(func_dict, package)
 
-    if inspect.isasyncgenfunction(func_attr) or inspect.iscoroutinefunction(func_attr):
-        result = await func_attr(**func_dict)
-    else:
-        result = func_attr(**func_dict)
+        if inspect.isasyncgenfunction(func_attr) or inspect.iscoroutinefunction(func_attr):
+            result = await func_attr(**func_dict)
+        else:
+            result = func_attr(**func_dict)
+        result_no_enum = enum_to_value(result)
 
-    return json(enum_to_value(result))
+    except Exception as e:
+        wrap_result = dict()
+        wrap_result["code"] = -1
+        wrap_result["message"] = str(e)
+        return json(wrap_result)
+
+    wrap_result = dict()
+    wrap_result["code"] = 0
+    wrap_result["data"] = result_no_enum
+    return json(wrap_result)
 
 
 @app.get("addListener/<package:str>/<clazz:str>/<event_type:str>")
